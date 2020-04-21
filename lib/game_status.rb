f1fa86d338f65f566941ebdfebaf9795be71162d
idith almog <almog.idith@gmail.com>
2020-04-21 17:47:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 def won?(board)
-WIN_COMBINATIONS.detect {|win_combination| 
+WIN_COMBINATIONS.detect {|win_combination|
   (board[win_combination[0]]="X" &&
   board[win_combination[1]]="X" &&
   board[win_combination[2]]="X") ||
